ae562e0a919c87a4815d4c8256a52361ad98c395
Byron Ron H <amendicovobis29@gmail.com>
2022-11-12 16:08:16 -0500

    Revert "feature: car db integration"
    This reverts commit 20c5c944a18711dee2a818764d8547722eb2a035.

--- src/components/Announcement.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../styles/Announcement.module.css";
-
-const Announcement = () => {
-  return <div className={styles.container}>Super Deal! Free Shipping</div>;
-};
-
-export default Announcement;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { AuthContextProvider } from "./context/AuthContext";
-import axios from "axios";
-
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
